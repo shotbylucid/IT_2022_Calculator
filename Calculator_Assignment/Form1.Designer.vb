201afdb0593195ec1e4fea69e03138e77e9d693a
LucidsHQ <lucidshq@gmail.com>
2022-11-21 16:09:11 +0930

    Just added all the basic function of the Calculator to the Code.

--- Calculator_Assignment/Form1.Designer.vb
+++ Calculator_Assignment/Form1.Designer.vb
@@ -22,89 +22,121 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
-        Me.Button6 = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnSub = New System.Windows.Forms.Button()
+        Me.btnMulti = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnAll = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.txtInput1 = New System.Windows.Forms.TextBox()
+        Me.txtInput2 = New System.Windows.Forms.TextBox()
+        Me.txtAns = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnAdd
         '
-        Me.Button1.Location = New System.Drawing.Point(785, 122)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(150, 46)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnAdd.Location = New System.Drawing.Point(586, 297)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(150, 46)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "Addition"
+        Me.btnAdd.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnSub
         '
-        Me.Button2.Location = New System.Drawing.Point(962, 122)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(150, 46)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnSub.Location = New System.Drawing.Point(763, 297)
+        Me.btnSub.Name = "btnSub"
+        Me.btnSub.Size = New System.Drawing.Size(150, 46)
+        Me.btnSub.TabIndex = 1
+        Me.btnSub.Text = "Subtract"
+        Me.btnSub.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btnMulti
         '
-        Me.Button3.Location = New System.Drawing.Point(1148, 122)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(150, 46)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnMulti.Location = New System.Drawing.Point(949, 297)
+        Me.btnMulti.Name = "btnMulti"
+        Me.btnMulti.Size = New System.Drawing.Size(150, 46)
+        Me.btnMulti.TabIndex = 2
+        Me.btnMulti.Text = "Multiply"
+        Me.btnMulti.UseVisualStyleBackColor = True
         '
-        'Button4
+        'btnClear
         '
-        Me.Button4.Location = New System.Drawing.Point(785, 196)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(150, 46)
-        Me.Button4.TabIndex = 3
-        Me.Button4.Text = "Button4"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.btnClear.Location = New System.Drawing.Point(586, 371)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(150, 46)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
         '
-        'Button5
+        'btnAll
         '
-        Me.Button5.Location = New System.Drawing.Point(962, 196)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(150, 46)
-        Me.Button5.TabIndex = 4
-        Me.Button5.Text = "Button5"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.btnAll.Location = New System.Drawing.Point(763, 371)
+        Me.btnAll.Name = "btnAll"
+        Me.btnAll.Size = New System.Drawing.Size(150, 46)
+        Me.btnAll.TabIndex = 4
+        Me.btnAll.Text = "All"
+        Me.btnAll.UseVisualStyleBackColor = True
         '
-        'Button6
+        'btnClose
         '
-        Me.Button6.Location = New System.Drawing.Point(1148, 196)
-        Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(150, 46)
-        Me.Button6.TabIndex = 5
-        Me.Button6.Text = "Button6"
-        Me.Button6.UseVisualStyleBackColor = True
+        Me.btnClose.Location = New System.Drawing.Point(949, 371)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(150, 46)
+        Me.btnClose.TabIndex = 5
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'txtInput1
+        '
+        Me.txtInput1.Location = New System.Drawing.Point(634, 225)
+        Me.txtInput1.Name = "txtInput1"
+        Me.txtInput1.Size = New System.Drawing.Size(200, 39)
+        Me.txtInput1.TabIndex = 6
+        '
+        'txtInput2
+        '
+        Me.txtInput2.Location = New System.Drawing.Point(858, 225)
+        Me.txtInput2.Name = "txtInput2"
+        Me.txtInput2.Size = New System.Drawing.Size(200, 39)
+        Me.txtInput2.TabIndex = 7
+        '
+        'txtAns
+        '
+        Me.txtAns.Location = New System.Drawing.Point(740, 469)
+        Me.txtAns.Name = "txtAns"
+        Me.txtAns.ReadOnly = True
+        Me.txtAns.Size = New System.Drawing.Size(200, 39)
+        Me.txtAns.TabIndex = 7
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1645, 840)
-        Me.Controls.Add(Me.Button6)
-        Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtAns)
+        Me.Controls.Add(Me.txtInput2)
+        Me.Controls.Add(Me.txtInput1)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnAll)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnMulti)
+        Me.Controls.Add(Me.btnSub)
+        Me.Controls.Add(Me.btnAdd)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
-    Friend WithEvents Button5 As Button
-    Friend WithEvents Button6 As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSub As Button
+    Friend WithEvents btnMulti As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnAll As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents txtInput1 As TextBox
+    Friend WithEvents txtInput2 As TextBox
+    Friend WithEvents txtAns As TextBox
 End Class
--- Calculator_Assignment/Form1.vb
+++ Calculator_Assignment/Form1.vb
@@ -1,3 +1,67 @@
 ﻿Public Class Form1
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim n1, n2, total As Decimal
 
+        '--Inputs
+        n1 = txtInput1.Text()
+        n2 = txtInput2.Text()
+
+        '--Process
+        total = n1 + n2
+
+        '--Ouput
+        txtAns.Text() = total
+
+    End Sub
+
+    Private Sub btnSub_Click(sender As Object, e As EventArgs) Handles btnSub.Click
+        Dim n1, n2, total As Decimal
+
+        '--Inputs
+        n1 = txtInput1.Text()
+        n2 = txtInput2.Text()
+
+        '--Process
+        total = n1 - n2
+
+        '--Ouput
+        txtAns.Text() = total
+
+    End Sub
+
+    Private Sub btnMulti_Click(sender As Object, e As EventArgs) Handles btnMulti.Click
+        Dim n1, n2, total As Decimal
+
+        '--Inputs
+        n1 = txtInput1.Text()
+        n2 = txtInput2.Text()
+
+        '--Process
+        total = n1 * n2
+
+        '--Ouput
+        txtAns.Text() = total
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        txtInput1.Clear()
+        txtInput2.Clear()
+        txtAns.Clear()
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+
+        Close()
+
+    End Sub
+
+    Private Sub btnAll_Click(sender As Object, e As EventArgs) Handles btnAll.Click
+
+        btnAdd_Click(sender, e)
+        btnSub_Click(sender, e)
+        btnMulti_Click(sender, e)
+
+    End Sub
 End Class
